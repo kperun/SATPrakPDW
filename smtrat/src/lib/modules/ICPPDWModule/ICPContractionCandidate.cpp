f3f810531933745dacb254d541607b7afcd5e2de
David Wlazlo <david.j.wlazlo@gmail.com>
2017-05-10 22:25:49 +0200

    Fully implemented contraction.

--- smtrat/src/lib/modules/ICPPDWModule/ICPContractionCandidate.cpp
+++ smtrat/src/lib/modules/ICPPDWModule/ICPContractionCandidate.cpp
@@ -39,18 +39,38 @@ namespace smtrat
     }
 
     std::pair<IntervalT,IntervalT> ICPContractionCandidate::getContractedInterval(const vb::VariableBounds<FormulaT>& _bounds) {
-        //first retrieve all variables with their respective bounds
+        // first retrieve all variables with their respective bounds
     	auto& map = _bounds.getIntervalMap();
 
-        //possible are two intervals resulting from a split
+        // possible are two intervals resulting from a split
+        IntervalT originalInterval = map.at(mVariable);
         IntervalT resultA, resultB;
 
         // apply contraction
         // arguments are true because we want to use propagation
     	bool split = mContractor(map, mVariable, resultA, resultB, true, true);
 
-        // TODO: the contractor thinks the constraints are equalities (poly = 0)
-        //       but we can have <= as well. so deal with that (new rules, see slides)
+        // we need a case distinction for different relations
+        // according to ICP slides from the lecture
+        if (mConstraint.relation() == carl::Relation::EQ) {
+            resultA = resultA.intersect(originalInterval);
+            resultB = resultB.intersect(originalInterval);
+        }
+        else if(mConstraint.relation() == carl::Relation::LEQ) {
+            // TODO: check if this is correct
+            resultA = IntervalT(originalInterval.lower(), 
+                                carl::getStrictestBoundType(originalInterval.lowerBoundType(), resultA.lowerBoundType()),
+                                min(originalInterval.upper(), resultA.upper()), 
+                                carl::getStrictestBoundType(originalInterval.upperBoundType(), resultA.upperBoundType()));
+            resultB = IntervalT(originalInterval.lower(), 
+                                carl::getStrictestBoundType(originalInterval.lowerBoundType(), resultB.lowerBoundType()),
+                                min(originalInterval.upper(), resultB.upper()), 
+                                carl::getStrictestBoundType(originalInterval.upperBoundType(), resultB.upperBoundType()));
+        }
+        else {
+            // We don't need that because it's always == or <= ??
+        }
+
         std::pair <IntervalT,IntervalT> ret(resultA,resultB);
         return ret;
     }
