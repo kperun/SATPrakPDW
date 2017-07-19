a38e25b1ea56fca1c725b1c6a0a37cb1237f6d44
KPerun <konstantin.perun@rwth-aachen.de>
2017-07-19 18:20:33 +0200

    Visualization for small problem added

--- smtrat/data_mining/visualizer.py
+++ smtrat/data_mining/visualizer.py
@@ -1,38 +1,31 @@
 import matplotlib.pyplot as plt
+import matplotlib.lines as mlines
 
-lines = [[comp for comp in (line.rstrip('\n')).replace('==',';').split(';')] for line in open('results.res')]
+lines = [[float(comp) for comp in (line.rstrip('\n')).replace('==',';').split(';')] for line in open('results.res')]
+
+
+curOpt = 1000
+for l in lines: 
+    if curOpt > l[8]:
+        print "set from" + str(curOpt) + " to " +str(l[8])
+        curOpt = l[8]
+        opt = l        
+
+print opt
+
+toShow = 0
+
+attributes = ['maxContractions','targetDiameter','maxSplitNumber','gainThreshold','alpha','weightEps','minCandidates','updateFactor']
+
 # you may also want to remove whitespace characters like `\n` at the end of each line
 #Schema: maxContractions;targetDiameter;maxSplitNumber;gainThreshold;alpha;weightEps;minCandidates;updateFactor;
 plt.grid(True)
-plt.subplot(421)
-plt.loglog([l[0] for l in lines],[l[8] for l in lines])
-plt.title('LogLog:MaxContractions vs. Time needed in seconds')
-#now targetDiameter vs. time 
-plt.subplot(422)
-plt.loglog([l[1] for l in lines],[l[8] for l in lines])
-plt.title('LogLog:TargetDiameter vs. Time needed in seconds')
-#now maxSplitNumber vs. time needed
-plt.subplot(423)
-plt.loglog([l[2] for l in lines],[l[8] for l in lines])
-plt.title('LogLog:MaxSplitNumber vs. Time needed in seconds')
-#now GainThreshold vs. time needed
-plt.subplot(424)
-plt.loglog([l[3] for l in lines],[l[8] for l in lines])
-plt.title('LogLog:GainThreshold vs. Time needed in seconds')
-#now Alpha vs. time needed
-plt.subplot(425)
-plt.loglog([l[4] for l in lines],[l[8] for l in lines])
-plt.title('LogLog: Alpha vs. Time needed in seconds')
-#now weightEps vs. time needed
-plt.subplot(426)
-plt.loglog([l[5] for l in lines],[l[8] for l in lines])
-plt.title('LogLog: weightEps vs. Time needed in seconds')
-#now minCandidates vs. time needed
-plt.subplot(427)
-plt.loglog([l[6] for l in lines],[l[8] for l in lines])
-plt.title('LogLog: minCandidates vs. Time needed in seconds')
-#now updateFactor vs. time needed
-plt.subplot(428)
-plt.loglog([l[7] for l in lines],[l[8] for l in lines])
-plt.title('LogLog: updateFactor vs. Time needed in seconds')
+#plt.subplot(421)
+plt.plot([l[toShow] for l in lines],[l[8] for l in lines])
+plt.plot([opt[toShow]],[opt[8]],'ro')
+blue_line = mlines.Line2D(range(1), range(1), color='white', marker='o',markerfacecolor='red',
+                                  markersize=10, label='Optimal '+attributes[toShow] + ' : ' + str(opt[toShow]) )
+plt.legend(handles=[blue_line])
+plt.title(attributes[toShow]+' vs. Time needed in seconds' )
+
 plt.show()
--- smtrat/script.py
+++ smtrat/script.py
@@ -1,26 +1,45 @@
 f = open('params.par','w+')
 f.write("#------------------------------------------------------------------------------------------------------------------------------------\n")
 f.write("#Schema: maxContractions;targetDiameter;maxSplitNumber;gainThreshold;alpha;weightEps;minCandidates;updateFactor;\n")
-maxContractions = 1000
-targetDiameter = 0.1
-maxSplitNumber = 100
-gainThreshold = 0.5
+maxContractions = 10 #updated from 1000 to 10
+targetDiameter = 0.141
+maxSplitNumber = 410
+gainThreshold = 0.41
 lowerDelta = 0.0
 upperDelta = 0.03
 alpha = 0.9
 weightEps = 0.5
-minCandidates = 5
+minCandidates = 5.0
 updateFactor = 1.1
+#10;0.141;410;0.41;0.9;0.5;5;1.1
+#number ofsamplings
+n = 1000
 
 
-for i in range(1,1000+1):
+#updates
+maxContractionsUpdate = maxContractions/n
+targetDiameterUpdate = targetDiameter/n 
+maxSplitNumberUpdate = 10*maxSplitNumber/n
+gainThresholdUpdate = gainThreshold/n
+#lowerDeltaUpdate = 0.0
+#upperDeltaUpdate = 0.03
+alphaUpdate = alpha/n
+weightEpsUpdate = weightEps/n
+minCandidatesUpdate = 1
+updateFactorUpdate = updateFactor/n
+
+#this bit-vector is used to represents which updates are active
+active = [0,0,0,0,0,0,0,0]
+
+
+
+for i in range(1,n+1):
     f.write(str(str(maxContractions) + ";"+str(targetDiameter) + ";" + str(maxSplitNumber) + ";" + str(gainThreshold) + ";"+str(alpha) + ";"+str(weightEps) + ";" + str(minCandidates) + ";" + str(updateFactor)+";\n"))
-    maxContractions -= 1
-    targetDiameter += 0.005
-    maxSplitNumber += 1
-    gainThreshold -= 0.0005
-    alpha -= 0.00089
-    weightEps -= 0.0005
-    minCandidates += 0.01*i
-    minCandidates = int(minCandidates)
-    updateFactor -= 0.001
+    maxContractions -= maxContractionsUpdate*active[0] 
+    targetDiameter += targetDiameterUpdate*active[1]
+    maxSplitNumber += maxSplitNumberUpdate*active[2]
+    gainThreshold -= gainThresholdUpdate*active[3]
+    alpha -= alphaUpdate*active[4]
+    weightEps -= weightEpsUpdate*active[5]
+    minCandidates += minCandidatesUpdate*active[6] 
+    updateFactor -= updateFactorUpdate*active[7]
